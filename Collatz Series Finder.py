e5c9b3cfd0969a6c32ea85ec40acae92cab5b11c
Özgür Soysal <o.soysal.factoreminv@gmail.com>
2020-08-12 11:55:01 +0300

    Update Collatz Series Finder.py
    Changed all variable names.

--- Collatz Series Finder.py
+++ Collatz Series Finder.py
@@ -1,42 +1,39 @@
 print("Collatz Series Finder")
 
-mogrnu = input("The largest number to be tested:  ")
+endnumber = int(input("The largest number to be tested:  "))
 
+num = 1
+unum = 1
 
-bitissayisi = int(mogrnu)
-
-sayi = 1
-usayi = 1
-
-sit = sayi % 2
+sit = num % 2
 
 i = 0
-kayitsayi = 0
-kayitsefer = 0
+recnum = 0
+reclenght = 0
 
-while usayi < bitissayisi:
-    usayi = usayi + 1
-    sayi = usayi
-    sit = sayi % 2
-    print(sayi)
-    while (sayi != 1) and (sayi != 0):
+while unum < endnumber:
+    unum = unum + 1
+    num = unum
+    sit = num % 2
+    print(num)
+    while (num != 1) and (num != 0):
 
-        while (sit == 1) and (sayi != 0):
-            sayi = sayi * 3 + 1
+        while (sit == 1) and (num != 0):
+            num = num * 3 + 1
             i = i + 1
-            sit = sayi % 2
+            sit = num % 2
 
-        while (sit == 0) and (sayi != 0):
-            sayi = sayi / 2
+        while (sit == 0) and (num != 0):
+            num = num / 2
             i = i + 1
-            sit = sayi % 2
+            sit = num % 2
 
-    if i > kayitsefer:
-        kayitsefer = i
-        kayitsayi = usayi
+    if i > reclenght:
+        reclenght = i
+        recnum = unum
         i = 0
     else :
         i = 0
 
-print(kayitsayi)
-print(kayitsefer)
+print(recnum)
+print(reclenght)
